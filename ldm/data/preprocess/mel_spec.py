dcf505a938f79b9e5e6a9aa9058c7d7cccdd98c4
liuhuadai <2285498557@qq.com>
2024-12-19 20:48:54 +0800

    Update mel_spec.py

--- ldm/data/preprocess/mel_spec.py
+++ ldm/data/preprocess/mel_spec.py
@@ -1,10 +1,11 @@
-from ldm.data.preprocess.NAT_mel import MelNet
+from preprocess.NAT_mel import MelNet
 import os
 from tqdm import tqdm
 from glob import glob
 import math
 import pandas as pd
-import logging
+import argparse
+from argparse import Namespace
 import math
 import audioread
 from tqdm.contrib.concurrent import process_map
@@ -15,8 +16,6 @@ import numpy as np
 from torch.distributed import init_process_group
 from torch.utils.data import Dataset,DataLoader,DistributedSampler
 import torch.multiprocessing as mp
-from argparse import Namespace
-from multiprocessing import Pool
 import json
 
 
@@ -53,7 +52,6 @@ class tsv_dataset(Dataset):
             temp_wav[:, :wav_length] = wav
         return temp_wav
 
-
     def __getitem__(self, index):
         audio_path = self.audio_paths[index]
         wav, orisr = torchaudio.load(audio_path)
@@ -77,12 +75,11 @@ def process_audio_by_tsv(rank,args):
     loader = DataLoader(dataset, sampler=sampler,batch_size=1, num_workers=16,drop_last=False)
 
     device = torch.device('cuda:{:d}'.format(rank))
-
     mel_net = MelNet(args.__dict__)
     mel_net.to(device)
     # if args.num_gpus > 1: # RuntimeError: DistributedDataParallel is not needed when a module doesn't have any parameter that requires a gradient.
     #     mel_net = DistributedDataParallel(mel_net, device_ids=[rank]).to(device)
-    
+    root = args.save_path
     loader = tqdm(loader) if rank == 0 else loader
     for batch in loader:
         audio_paths,wavs = batch
@@ -90,7 +87,7 @@ def process_audio_by_tsv(rank,args):
         if args.save_resample:               
             for audio_path,wav in zip(audio_paths,wavs):
                 psplits = audio_path.split('/')
-                root,wav_name = psplits[0],psplits[-1]
+                wav_name = psplits[-1]
                 # save resample
                 resample_root,resample_name = root+f'_{sr}',wav_name[:-4]+'_audio.npy'
                 resample_dir_name = os.path.join(resample_root,*psplits[1:-1])
@@ -104,9 +101,9 @@ def process_audio_by_tsv(rank,args):
 
             for audio_path,wav in zip(audio_paths,wavs):
                 psplits = audio_path.split('/')
-                root,wav_name = psplits[0],psplits[-1]
-                mel_root,mel_name = root+f'_mel{mode}{sr}nfft{args.fft_size}',wav_name[:-4]+'_mel.npy'
-                mel_dir_name = os.path.join(mel_root,*psplits[1:-1])
+                wav_name = psplits[-1]
+                mel_root,mel_name = root,wav_name[:-4]+'_mel.npy'
+                mel_dir_name = os.path.join(mel_root,f'mel{mode}{sr}',*psplits[1:-1])
                 mel_path = os.path.join(mel_dir_name,mel_name)
                 if not os.path.exists(mel_path):
                     mel_spec = mel_net(wav).cpu().numpy().squeeze(0) # (mel_bins,mel_len) 
@@ -161,32 +158,58 @@ def drop_bad_wavs(tsv_path):# 'audioset.csv'
     df = df.drop(bad_indices,axis=0)
     df.to_csv(tsv_path,sep='\t',index=False)
 
+def addmel2tsv(save_dir,tsv_path):
+    df = pd.read_csv(tsv_path,sep='\t')
+    mels = glob(f'{save_dir}/mel{args.mode}{args.audio_sample_rate}/**/*_mel.npy',recursive=True)
+    name2mel,idx2name,idx2mel = {},{},{}
+    for mel in mels:
+        bn = os.path.basename(mel)[:-8]# remove _mel.npy
+        name2mel[bn] = mel
+    for t in df.itertuples():
+        idx = int(t[0])
+        bn = os.path.basename(getattr(t,'audio_path'))[:-4]
+        idx2name[idx] = bn
+    for k,v in idx2name.items():
+        idx2mel[k] = name2mel[v]
+    df['mel_path'] = df.index.map(idx2mel)
+    df.to_csv(tsv_path,sep='\t',index=False)
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( "--tsv_path",type=str)
+    parser.add_argument( "--num_gpus",type=int,default=1)
+    parser.add_argument( "--max_duration",type=int,default=30)
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='example.log',  level=logging.INFO,
-        format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-    tsv_path = './musiccap.tsv'
+    pargs = parse_args()
+    tsv_path = pargs.tsv_path
     if os.path.isdir(tsv_path):
         files = glob(os.path.join(tsv_path,'*.tsv'))
         for file in files:
             drop_bad_wavs(file)
     else:
         drop_bad_wavs(tsv_path)
-    num_gpus = 1
+    num_gpus = pargs.num_gpus
+    batch_max_length = int(pargs.max_duration * 62.5)# 62.5 is the mel length for 1 second
+    save_path = 'processed'
     args = {
         'audio_sample_rate': 16000,
         'audio_num_mel_bins':80,
-        'fft_size': 1024,# 4000:512 ,16000:1024,
+        'fft_size': 1024,
         'win_size': 1024,
         'hop_size': 256,
         'fmin': 0,
         'fmax': 8000,
-        'batch_max_length': 1560, # 4000:312 (nfft = 512,hoplen=128,mellen = 313), 16000:624 , 22050:848 # 
+        'batch_max_length': batch_max_length, 
         'tsv_path': tsv_path,
         'num_gpus': num_gpus,
-        'mode': 'none',
+        'mode': 'none', # pad,none,
         'save_resample':False,
-        'save_mel' :True
+        'save_mel' :True,
+        'save_path': save_path,
     }
+    os.makedirs(save_path,exist_ok=True)
     args = Namespace(**args)  
     args.dist_config = {
         "dist_backend": "nccl",
@@ -197,5 +220,7 @@ if __name__ == '__main__':
         mp.spawn(process_audio_by_tsv,nprocs=args.num_gpus,args=(args,))
     else:
         process_audio_by_tsv(0,args=args)
+    print("proceoss mel done")
+    addmel2tsv(save_path,tsv_path)
     print("done")
     
